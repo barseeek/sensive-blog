f07a87f30afa654afb580db4477adcf35f202de3
Nikita <barskii.no@outlook.com>
2024-02-02 10:58:21 +0300

    prefetch posts count for tags

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Count
+from django.db.models import Count, Prefetch
 from django.shortcuts import render
 from blog.models import Comment, Post, Tag
 
@@ -30,7 +30,7 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all().annotate(posts_count=Count('posts'))],
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -45,11 +45,11 @@ def serialize_tag(tag):
 def index(request):
     most_popular_posts = Post.objects.popular() \
         .prefetch_related('author')[:5] \
-        .prefetch_related('tags') \
+        .prefetch_related(Prefetch('tags', queryset= Tag.objects.all().annotate(posts_count=Count('posts')))) \
         .fetch_with_comments_count()
     fresh_posts = (Post.objects.annotate(comments_count=Count('comments', distinct=True)) \
         .prefetch_related('author') \
-        .prefetch_related('tags') \
+        .prefetch_related(Prefetch('tags', queryset= Tag.objects.all().annotate(posts_count=Count('posts'))))\
         .order_by('published_at'))
     most_fresh_posts = list(fresh_posts)[-5:]
     most_popular_tags = Tag.objects.popular() \
